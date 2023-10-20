c165befd5a3ccc7065a073199f34e1753a21b15a
Ana Garcia Lopez <angarlopez@hotmail.com>
2023-10-20 10:52:33 +0200

    empezando a hacer pizza

---
+++ app.js
@@ -0,0 +1,20 @@
+//Ingredientes
+let masaDePizza = "masa de pizza congelada";
+let salsaDeTomate = "Salsa de tomate";
+let queso = "Queso Mozarella";
+let jamon = "jamón york";
+let piña = "doble de piña";
+let champiñones = "champiñones";
+let ingredientesAdiciones = ["pimiento", "salsa barbacoa"];
+let comprarMasa = "comprar masa fresca";
+let temperatura = " horno a 220C";
+let amasado = "amasar la masa durante 5 minutos";
+let extenderMasa = "dar forma plana y redonda a la masa y llevarla a una bandeja de horno";
+let ingredientes = "extender una base de tomate, queso, jamón, piña y champiñones. Puedes adiccionar extras a tu gusto";
+let horneadoPizza = "llevar la bandeja al horno durante 20 minutos";
+let pizzaLista = "Sacar la pizza del horno, cuando el borde esté algo tostado, cortar y disfrutar :)";
+
+//Instrucciones
+console.log("Receta de Pizza tropical");
+
+console.log("1. Ir al supermercado de confianza a comprar" + masaDePizza + "los siguientes ingredientes:" + )
