3d67030f2949bdc91b4a5c28d37321d3c4adbfd4
Ana Garcia Lopez <angarlopez@hotmail.com>
2023-10-23 10:19:59 +0200

    introducion de app en la pizza

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
 //Ingredientes
-let masaDePizza = "masa de pizza congelada";
-let salsaDeTomate = "Salsa de tomate";
-let queso = "Queso Mozarella";
-let jamon = "jamón york";
-let piña = "doble de piña";
-let champiñones = "champiñones";
-let ingredientesAdiciones = ["pimiento", "salsa barbacoa"];
-let comprarMasa = "comprar masa fresca";
-let temperatura = " horno a 220C";
-let amasado = "amasar la masa durante 5 minutos";
-let extenderMasa = "dar forma plana y redonda a la masa y llevarla a una bandeja de horno";
-let ingredientes = "extender una base de tomate, queso, jamón, piña y champiñones. Puedes adiccionar extras a tu gusto";
-let horneadoPizza = "llevar la bandeja al horno durante 20 minutos";
-let pizzaLista = "Sacar la pizza del horno, cuando el borde esté algo tostado, cortar y disfrutar :)";
+let masaPizza = "masa fresca";
+let salsaDeTomate = "Salsa de tomate, ";
+let comprarIngredientes = "queso mozarella, jamón, piña, champi. ";
+let extra = "salsa barbacoa";
+let temperatura = " 220C";
+let amasar = "amasar la masa durante 5 minutos";
+let extenderMasa = "dar forma plana y redonda a la masa";
+let ingredientes = "poner una base de tomate, queso, jamón, piña y champiñones."
+let horneadoPizza = "Llevar la pizza al horno durante 20 minutos. ";
+let pizzaLista = "Sacar la pizza del horno, cuando el borde esté algo tostado, cortar y disfrutar.";
 
 //Instrucciones
-console.log("Receta de Pizza tropical");
+function comprarMasa() {
+    console.log("1. Ir al supermercado a comprar una: " + masaDePizza +  ", y conseguir los siguientes ingredientes: " + comprarIngredientesngredientes)
+}
 
-console.log("1. Ir al supermercado de confianza a comprar" + masaDePizza + "los siguientes ingredientes:" + )
+function preHeat() {
+    console.log("2. Precalentar el horno a " + temperatura + ".");
+}
+
+function amasado() {
+    console.log("3. A continuación " + amasar + " y después " + extenderMasa " y " + ingredientes);
+}
+
+function add() {
+    console.log("4. Puedes añadir " + extra + " al gusto.");
+}
+
+function heat() {
+    console.log("5. " + horneadoPizza);
+}
+
+function comer() {
+    console.log("6. " + pizzaLista);
+}
+
+app();
+comprarMasa();
+preHeat();
+amasado();
+add();
+heat();
+comer();
