2a3f248f5ddeb41b3d129993474c7ad7f37e4af5
Ana Garcia Lopez <angarlopez@hotmail.com>
2023-10-30 11:38:45 +0100

    desarrollo del novel 2 con prompt-sync

--- level2/DeliverPizza.js
+++ level2/DeliverPizza.js
@@ -1,10 +1,5 @@
-// const prompt = require(´prompt-sync´)({sigint:true});
-
-import PromptSync from "prompt-sync";
-
-import { DeliverPizza } from "./level2/DeliverPizza";
-
-function app() {
-    const prompt = PromptSync();
-    const pizzaName = prompt ("And your pizza is...(tropical): ");
+export function deliverPizza(selectedPizza) {
+    for (let i = 0; 1 < selectedPizza.ingredients.length; 1++) {
+        console.log("Add " + selectedPizza.ingredients[i] + ".");
+    }
 }
---
+++ level2/app.js
@@ -0,0 +1,9 @@
+import PromptSync from "prompt-sync";
+import { choosePizza } from "./modules/choosePizza.js";
+
+function app() {
+    const prompt = PromptSync();
+    const pizzaName = prompt("What pizza do you want? (tropical, barbacoa):");
+}
+
+app()
